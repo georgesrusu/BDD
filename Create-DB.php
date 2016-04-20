8d0fe9fc065ed25ff38d95361e499a6ef0d71ef5
georgesrusu <georgesrusu@yahoo.fr>
2016-04-20 16:36:13 +0200

    fin creation DB

--- Create-DB.php
+++ Create-DB.php
@@ -23,7 +23,7 @@
 //Creation de table Commentaire	
 	echo "Creating table Commentaire status: ";
 	try {
-    	$sql = "CREATE TABLE Commentaire (etablissementID INT UNSIGNED NOT NULL,clientID INT UNSIGNED NOT NULL,dateCreation DATETIME,texte VARCHAR(300) NOT NULL,score INT(1) NOT NULL,PRIMARY KEY (etablissementID,clientID,dateCreation))";
+    	$sql = "CREATE TABLE Commentaire (etablissementID INT UNSIGNED NOT NULL,clientID INT UNSIGNED NOT NULL,dateCreation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,texte VARCHAR(300) NOT NULL,score INT(1) NOT NULL,PRIMARY KEY (etablissementID,clientID,dateCreation))";
     // use exec() because no results are returned
     	$conn->exec($sql);
     	echo "Table created successfully<br>";
@@ -36,7 +36,7 @@
 
 	echo "Creating table Utilisateur status: ";
 	try {
-    	$sql = "CREATE TABLE Utilisateur (ID INT UNSIGNED NOT NULL,identifiant VARCHAR(30) NOT NULL,mot_de_passe VARCHAR(30) NOT NULL,email VARCHAR(50) NOT NULL,dateCreation DATETIME,isAdmin BOOLEAN NOT NULL,PRIMARY KEY (id,identifiant))";
+    	$sql = "CREATE TABLE Utilisateur (ID INT UNSIGNED NOT NULL,identifiant VARCHAR(30) NOT NULL,mot_de_passe VARCHAR(30) NOT NULL,email VARCHAR(50) NOT NULL,dateCreation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,isAdmin BOOLEAN NOT NULL DEFAULT 0,PRIMARY KEY (id,identifiant))";
     // use exec() because no results are returned
     	$conn->exec($sql);
     	echo "Table created successfully<br>";
@@ -49,7 +49,7 @@
 
 	echo "Creating table Tag status: ";
 	try {
-    	$sql = "CREATE TABLE Tag (ID INT UNSIGNED NOT NULL,label VARCHAR(30) NOT NULL,etablissementID INT UNSIGNED NOT NULL,clientID INT UNSIGNED NOT NULL,PRIMARY KEY (id,label,etablissementID,clientID))";
+    	$sql = "CREATE TABLE Tag (ID INT UNSIGNED NOT NULL,label VARCHAR(30) NOT NULL,etablissementID INT UNSIGNED NOT NULL,clientID INT UNSIGNED NOT NULL,PRIMARY KEY (ID,label,etablissementID,clientID))";
     // use exec() because no results are returned
     	$conn->exec($sql);
     	echo "Table created successfully<br>";
@@ -59,5 +59,54 @@
     	echo "Error: " . $e->getMessage();
     	echo "<br>";
 	}
-	
+
+	echo "Creating table Etablissement status: ";
+	try {
+    	$sql = "CREATE TABLE Etablissement (ID INT UNSIGNED NOT NULL,nom VARCHAR(30) NOT NULL,rue VARCHAR(50) NOT NULL,numero INT UNSIGNED NOT NULL,codePostal INT UNSIGNED NOT NULL,localite VARCHAR(50) NOT NULL,longitude VARCHAR(10) NOT NULL,latitude VARCHAR(10) NOT NULL,telephone INT UNSIGNED NOT NULL,lienWeb VARCHAR(50),type VARCHAR(10) NOT NULL,PRIMARY KEY (ID,nom,type))";
+    // use exec() because no results are returned
+    	$conn->exec($sql);
+    	echo "Table created successfully<br>";
+		
+		}
+	catch(PDOException $e) {
+    	echo "Error: " . $e->getMessage();
+    	echo "<br>";
+	}
+			
+	echo "Creating table Bar status: ";
+	try {
+    	$sql = "CREATE TABLE Bar (ID INT UNSIGNED PRIMARY KEY NOT NULL,fumeur BOOLEAN NOT NULL,petiteRestauration BOOLEAN NOT NULL)";
+    // use exec() because no results are returned
+    	$conn->exec($sql);
+    	echo "Table created successfully<br>";
+		
+		}
+	catch(PDOException $e) {
+    	echo "Error: " . $e->getMessage();
+    	echo "<br>";
+	}
+	echo "Creating table Hotel status: ";
+	try {
+    	$sql = "CREATE TABLE Hotel (ID INT UNSIGNED PRIMARY KEY NOT NULL,prix FLOAT UNSIGNED NOT NULL,nbChambre INT UNSIGNED NOT NULL,nbEtoiles INT UNSIGNED NOT NULL)";
+    // use exec() because no results are returned
+    	$conn->exec($sql);
+    	echo "Table created successfully<br>";
+		
+		}
+	catch(PDOException $e) {
+    	echo "Error: " . $e->getMessage();
+    	echo "<br>";
+	}
+	echo "Creating table Restaurant status: ";
+	try {
+    	$sql = "CREATE TABLE Restaurant (ID INT UNSIGNED PRIMARY KEY NOT NULL,prix FLOAT UNSIGNED NOT NULL,placesBanquet INT NOT NULL,emporter BOOLEAN NOT NULL,livraison BOOLEAN NOT NULL,fermeture INT(6) UNSIGNED NOT NULL)";
+    // use exec() because no results are returned
+    	$conn->exec($sql);
+    	echo "Table created successfully<br>";
+		
+		}
+	catch(PDOException $e) {
+    	echo "Error: " . $e->getMessage();
+    	echo "<br>";
+	}
 ?>
