e1b3ea2106c76b4fb78bb6277fe92992bedef272
M. Romain <maximilienromain96@hotmail.com>
2016-05-07 15:09:21 +0200

    Update exemple.php

--- TemplateSite/exemple.php
+++ TemplateSite/exemple.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+//if(!isset($_SESSION['cart_items'])){
+    //$_SESSION['cart_items'] = array();
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <!--
 Design by TEMPLATED
@@ -23,14 +28,14 @@ Released   : 20130902
 <!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
 
 <?php
-	include("../connect.php");
+	include("connect.php");
 	$stmt = $conn->prepare("SELECT * FROM Etablissement"); 
 	$stmt->execute();
 
     // set the resulting array to associative
 	$result = $stmt->fetchall(); //fetch
 
-	$i = $_GET['id'];
+	$i = $_GET['etablissementID']-1;
 	$idEtablissement = $result[$i][0];
 	$typeEtablissement = $result[$i][10];
 
@@ -143,9 +148,18 @@ google.maps.event.addDomListener(window, 'load', initialize);
 		</div>
 		<div id="menu">
 			<ul>
-				<li class="current_page_item"><a href="#" accesskey="1" title="">Homepage</a></li>
-				<li><a href="#" accesskey="2" title="Type here to research something">Research</a></li>
-				<li><a href="../login.php" accesskey="3" title="Connexion to our database">Connexion</a></li>
+				<li><a href="./index.php" accesskey="1" title="">Homepage</a></li>
+				<li class="current_page_item"><a href="./search.php" accesskey="2" title="Type here to research something">Research</a></li>
+				<li><?php
+				if(isset($_SESSION['pseudo'])) {
+					echo '<a href="./profile.php" accesskey="3" title="Connexion to our database">';
+					echo "Profil";
+				}
+				else{
+					echo '<a href="./login.php" accesskey="3" title="Connexion to our database">';
+					echo "Connexion";
+				}
+				 ?></a></li>
 			</ul>
 		</div>
 	</div>
@@ -283,16 +297,15 @@ google.maps.event.addDomListener(window, 'load', initialize);
 					if ($labelList[$label][0] == $idEtablissement) {
 						if (!in_array($labelList[$label][2], $labelArray)) {
 							array_push($labelArray, $labelList[$label][2]);
-							array_push($countArray, 1);
+							array_push($countArray, (int)1);
 						}
 						else {
-							$id = array_keys($labelArray, $labelList[$label][2]);
-							$countArray[$id] = $countArray[$id] + 1;
+							$idArray = array_keys($labelArray, $labelList[$label][2]);
+							$countArray[$idArray[0]] = $countArray[$idArray[0]] + 1;
 						}
 					}
 				}
 				for ($label=0; $label < count($labelArray); $label++) { 
-					#echo "<p>" . $labelArray[$label] . "</p>";
 					echo "	<tr>
 								<td>" . $labelArray[$label] . "</td>
 								<td>" . $countArray[$label] . "</td>
