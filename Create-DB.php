16ef15c75526d4379f8ae193e2faca246c5b921d
georgesrusu <georgesrusu@yahoo.fr>
2016-05-10 01:59:15 +0200

    ajout des requete.txt

--- Create-DB.php
+++ Create-DB.php
@@ -21,7 +21,7 @@
     $conn->exec($sql);
 	echo "Creating table Utilisateur status: ";
 	try {
-    	$sql = "CREATE TABLE Utilisateur (ID INT UNSIGNED NOT NULL AUTO_INCREMENT,identifiant VARCHAR(30) NOT NULL UNIQUE,mot_de_passe VARCHAR(30) NOT NULL,email VARCHAR(50) NOT NULL,dateCreation DATE NOT NULL,isAdmin TINYINT(1) NOT NULL DEFAULT 0,PRIMARY KEY (id,identifiant))";
+    	$sql = "CREATE TABLE Utilisateur (ID INT UNSIGNED NOT NULL AUTO_INCREMENT,identifiant VARCHAR(30) NOT NULL UNIQUE,mot_de_passe VARCHAR(30) NOT NULL,email VARCHAR(50) NOT NULL,dateCreation DATE NOT NULL,isAdmin TINYINT(1) NOT NULL DEFAULT 0,PRIMARY KEY (id,identifiant))ENGINE=InnoDB";
     	$conn->exec($sql);
     	echo "Table created successfully<br>";
 		
@@ -34,7 +34,7 @@
 	echo "Creating table Etablissement status: ";
 	try {
 
-    	$sql = "CREATE TABLE Etablissement (ID INT UNSIGNED NOT NULL AUTO_INCREMENT,nom VARCHAR(50) NOT NULL UNIQUE,rue VARCHAR(50) NOT NULL,numero INT UNSIGNED NOT NULL,codePostal INT UNSIGNED NOT NULL,localite VARCHAR(30) NOT NULL,longitude FLOAT NOT NULL,latitude FLOAT NOT NULL,telephone VARCHAR(15) NOT NULL,lienWeb VARCHAR(100),type VARCHAR(10) NOT NULL,PRIMARY KEY (ID,nom,type))";
+    	$sql = "CREATE TABLE Etablissement (ID INT UNSIGNED NOT NULL AUTO_INCREMENT,nom VARCHAR(50) NOT NULL UNIQUE,rue VARCHAR(50) NOT NULL,numero INT UNSIGNED NOT NULL,codePostal INT UNSIGNED NOT NULL,localite VARCHAR(30) NOT NULL,longitude FLOAT NOT NULL,latitude FLOAT NOT NULL,telephone VARCHAR(15) NOT NULL,lienWeb VARCHAR(100),type VARCHAR(10) NOT NULL,PRIMARY KEY (ID,nom,type))ENGINE=InnoDB";
     	$conn->exec($sql);
     	echo "Table created successfully<br>";
 		
@@ -46,7 +46,7 @@
 			
 	echo "Creating table Bar status: ";
 	try {
-    	$sql = "CREATE TABLE Bar (ID INT UNSIGNED PRIMARY KEY NOT NULL,fumeur TINYINT(1) NOT NULL,petiteRestauration TINYINT(1) NOT NULL,FOREIGN KEY (ID) REFERENCES Etablissement(ID))";
+    	$sql = "CREATE TABLE Bar (ID INT UNSIGNED PRIMARY KEY NOT NULL,fumeur TINYINT(1) NOT NULL,petiteRestauration TINYINT(1) NOT NULL,FOREIGN KEY (ID) REFERENCES Etablissement(ID))ENGINE=InnoDB";
     	$conn->exec($sql);
     	echo "Table created successfully<br>";
 		
@@ -57,7 +57,7 @@
 	}
 	echo "Creating table Hotel status: ";
 	try {
-    	$sql = "CREATE TABLE Hotel (ID INT UNSIGNED PRIMARY KEY NOT NULL,prix FLOAT UNSIGNED NOT NULL,nbChambres INT UNSIGNED NOT NULL,nbEtoiles INT UNSIGNED NOT NULL,FOREIGN KEY (ID) REFERENCES Etablissement(ID))";
+    	$sql = "CREATE TABLE Hotel (ID INT UNSIGNED PRIMARY KEY NOT NULL,prix FLOAT UNSIGNED NOT NULL,nbChambres INT UNSIGNED NOT NULL,nbEtoiles INT UNSIGNED NOT NULL,FOREIGN KEY (ID) REFERENCES Etablissement(ID))ENGINE=InnoDB";
     	$conn->exec($sql);
     	echo "Table created successfully<br>";
 		
@@ -68,7 +68,7 @@
 	}
 	echo "Creating table Restaurant status: ";
 	try {
-    	$sql = "CREATE TABLE Restaurant (ID INT UNSIGNED PRIMARY KEY NOT NULL,prix FLOAT UNSIGNED NOT NULL,placesBanquet INT NOT NULL,emporter TINYINT(1) NOT NULL,livraison TINYINT(1) NOT NULL,fermeture VARCHAR(7) NOT NULL,FOREIGN KEY (ID) REFERENCES Etablissement(ID))";
+    	$sql = "CREATE TABLE Restaurant (ID INT UNSIGNED PRIMARY KEY NOT NULL,prix FLOAT UNSIGNED NOT NULL,placesBanquet INT NOT NULL,emporter TINYINT(1) NOT NULL,livraison TINYINT(1) NOT NULL,fermeture VARCHAR(7) NOT NULL,FOREIGN KEY (ID) REFERENCES Etablissement(ID))ENGINE=InnoDB";
     	$conn->exec($sql);
     	echo "Table created successfully<br>";
 		
@@ -79,7 +79,7 @@
 	}
 	echo "Creating table ModificationAdmin status: ";
 	try {
-    	$sql = "CREATE TABLE ModificationAdmin (etablissementID INT UNSIGNED NOT NULL,adminID INT UNSIGNED NOT NULL,dateCreation DATE NOT NULL,PRIMARY KEY (etablissementID,adminID,dateCreation),FOREIGN KEY (etablissementID) REFERENCES Etablissement(ID),FOREIGN KEY (adminID) REFERENCES Utilisateur(ID))";
+    	$sql = "CREATE TABLE ModificationAdmin (etablissementID INT UNSIGNED NOT NULL,adminID INT UNSIGNED NOT NULL,dateCreation DATE NOT NULL,PRIMARY KEY (etablissementID,adminID,dateCreation),FOREIGN KEY (etablissementID) REFERENCES Etablissement(ID),FOREIGN KEY (adminID) REFERENCES Utilisateur(ID))ENGINE=InnoDB";
     	$conn->exec($sql);
     	echo "Table created successfully<br>";
 		
@@ -90,7 +90,7 @@
 	}
 	echo "Creating table Label status: ";
 	try {
-    	$sql = "CREATE TABLE Label (etablissementID INT UNSIGNED NOT NULL,clientID INT UNSIGNED NOT NULL,texte VARCHAR(50) NOT NULL,PRIMARY KEY (etablissementID,clientID,texte),FOREIGN KEY (etablissementID) REFERENCES Etablissement(ID),FOREIGN KEY (clientID) REFERENCES Utilisateur(ID))";
+    	$sql = "CREATE TABLE Label (etablissementID INT UNSIGNED NOT NULL,clientID INT UNSIGNED NOT NULL,texte VARCHAR(50) NOT NULL,PRIMARY KEY (etablissementID,clientID,texte),FOREIGN KEY (etablissementID) REFERENCES Etablissement(ID),FOREIGN KEY (clientID) REFERENCES Utilisateur(ID))ENGINE=InnoDB";
     	$conn->exec($sql);
     	echo "Table created successfully<br>";
 		
@@ -102,7 +102,7 @@
 	//Creation de table Commentaire	
 	echo "Creating table Commentaire status: ";
 	try {
-    	$sql = "CREATE TABLE Commentaire (etablissementID INT UNSIGNED NOT NULL,clientID INT UNSIGNED NOT NULL,dateCreation DATE NOT NULL,texte VARCHAR(1000) NOT NULL,score INT(1) NOT NULL,PRIMARY KEY (etablissementID,clientID,dateCreation),FOREIGN KEY (etablissementID) REFERENCES Etablissement(ID),FOREIGN KEY (clientID) REFERENCES Utilisateur(ID))";
+    	$sql = "CREATE TABLE Commentaire (etablissementID INT UNSIGNED NOT NULL,clientID INT UNSIGNED NOT NULL,dateCreation DATE NOT NULL,texte VARCHAR(1000) NOT NULL,score INT(1) NOT NULL,PRIMARY KEY (etablissementID,clientID,dateCreation),FOREIGN KEY (etablissementID) REFERENCES Etablissement(ID),FOREIGN KEY (clientID) REFERENCES Utilisateur(ID))ENGINE=InnoDB";
     	$conn->exec($sql);
     	echo "Table created successfully<br>";
 		
